61dbba4c61bfd31474a335176887d0e56b1d47ce
LazyLEE1008 <66935871+LazyLEE1008@users.noreply.github.com>
2023-10-26 21:27:49 +0900

    add_Actor
    변수 스트링, int16으로 선언하는법 배움

---
+++ Source/UE5_Practice/MyActor.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class UE5_PRACTICE_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "damage")
+		int32 damage;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "damage")
+		float damage_time;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Transient, Category = "damage")
+		float damage_per_seconds;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FString charaterName;
+
+	bool bAttackable;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+public :
+	void CalculateDPS();
+
+
+
+};
--- Source/UE5_Practice/UE5_PracticeCharacter.cpp
+++ Source/UE5_Practice/UE5_PracticeCharacter.cpp
@@ -17,7 +17,7 @@ AUE5_PracticeCharacter::AUE5_PracticeCharacter()
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
 	// set our turn rate for input
-	TurnRateGamepad = 50.f;
+	TurnRateGamepad = 5.f;
 
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
